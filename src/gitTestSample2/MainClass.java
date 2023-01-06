b0aac33280fd277f1e302a159369eeb60bf4ec5f
kwatermelon <hanamango@naver.com>
2023-01-06 15:09:33 +0900

    replacecode

--- src/gitTestSample2/MainClass.java
+++ src/gitTestSample2/MainClass.java
@@ -5,6 +5,11 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		System.out.println("git test sample base");
+	
+		
+		System.out.println("수정하였음!!!!!");
 	}
 
 }
